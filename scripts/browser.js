9b23e40b44c8b24208efb06243e70e43782b7ed4
rjlee276 <46760932+rjlee276@users.noreply.github.com>
2022-08-06 20:56:11 -0700

    Delete scripts directory

--- scripts/browser.js
+++
@@ -1,9 +0,0 @@
-const { CPU } = require('../main/cpu')
-const { RomBuffer } = require('../main/ROM_buffer')
-const { webInterface } = require('../interfaces/web_interface')
-
-const cpuInterface = new webInterface
-const cpu = new CPU(cpuInterface)
-
-global.cpu = cpu
-global.RomBuffer = RomBuffer
