a48d4c4a7eefdc242fea227240d6f7aeaee26902
rjl275 <rjl275@cornell.edu>
2022-08-07 15:25:23 -0700

    initialcommit

--- web.js
+++ web.js
@@ -139,6 +139,16 @@ const hex = (value, length = 2) => {
     return padded.substr(padded.length - length)
 }
 
+function displayMemory() {
+    let address = 0x200
+    const clasz = address
+    const addresz = '0x' + hex(address, 4)
+    while (address < 4096) {
+        $('.panel1').append('<div>1</div>')
+        address += 2
+    }
+}
+
 function updateHighlight() {
     document.querySelector('.info > .panel1 > div').removeClass('pc')
 }
