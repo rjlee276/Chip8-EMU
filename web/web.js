8454665a6769101a920cfbc5505efcebea420d3b
rjl275 <rjl275@cornell.edu>
2022-08-07 05:06:06 -0700

    initialcommit

--- web/web.js
+++ web/web.js
@@ -1,3 +1,5 @@
+import $ from "jquery";
+
 let timer = 0
 function emulateCycle() {
     timer++
@@ -7,7 +9,10 @@ function emulateCycle() {
     }
 
     cpu.step()
-
+    updateHighlight()
+    displayMemory()
+    displayRegisters()
+    scrollBottom()
     setTimeout(emulateCycle, 2)
 }
 
@@ -28,55 +33,77 @@ function displayInstructions(rom) {
 
     switch (rom) {
         case 'CONNECT4':
-            instructions = `
-            Q = go to the left
-            E = go to the right
-            W = drop a coin
+            instructions = `Q = go to the left
+E = go to the right
+W = drop a coin
 
-            Coin suit alternates with each play. 
-            This game has no victory detection method.
-            `
+Coin suit alternates with each play. 
+This game has no victory detection.
+`
             break
 
         case 'TETRIS':
-            instructions = `
-            W = go to the left
-            E = go to the right 
-            R = fall faster 
-            Q = rotate block
-            `
+            instructions = `W = go to the left
+E = go to the right 
+R = fall faster 
+Q = rotate block
+`
             break
 
         case 'PONG':
-            instructions = `
-            Player 1: 
+            instructions = `Player 1: 
 
-            2 = go up
-            Q = go down
+2 = go up
+Q = go down
 
-            Player 2: 
+Player 2: 
 
-            2 = go up
-
-            X = go down
-            `
+2 = go up
+X = go down`
 
             break
 
         case 'INVADERS':
-            instructions = `
-            W = start game 
-            W = shoot
-            Q = go to the left
-            E = go to the right
-            `
+            instructions = `W = start game 
+W = shoot
+Q = go to the left
+E = go to the right`
             break
     }
 
     document.querySelector('.instructions').textContent = instructions
 }
 
+function scrollBottom() {
+    document.querySelector('panel1').scrollTop = document.querySelector('panel1').scrollHeight
+    document.querySelector('panel2').scrollTop = document.querySelector('panel2').scrollHeight
+}
+
+const hex = (value, length = 2) => {
+    const padded = '0000' + value.toString(16).toUpperCase()
+    return padded.substr(padded.length - length)
+}
+
+function displayMemory() {
+    const clasz = cpu.PC
+    const addresz = '0x' + hex(cpu.PC, 4)
+    $('panel1').append(`<div class='pc ${clasz}'>${addresz} - ${cpu.decode(cpu.fetch()).instruction.id}</div>`)
+}
+
+function updateHighlight() {
+    $('.info > .panel1 > div').removeClass('pc')
+}
+
+function displayRegisters() {
+    for (let i = 0; i < cpu.registers; i++) {
+        $('panel2').append(`<div>V${i}: ${cpu.registers[i]}</div>`)
+    }
+}
+
 document.querySelector('select').addEventListener('change', load)
 
 emulateCycle()
+
+
+
 
