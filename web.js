07ee040a3c4e49ddc69fc875ad50f422e2bd5b1a
rjl275 <rjl275@cornell.edu>
2022-08-08 18:53:46 -0700

    initialcommit

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ function emulateCycle(RomBuffer, list) {
         for (let i = 0; i < RomBuffer.data.length; i += 2) {
             list.splice(0x200, 0, cpu.decode(cpu.fetch())) //assign to some empty list
         }
-
+        console.log(list[cpu.PC])
         cpu.execute(list[cpu.PC])
 
         displayRegisters()
