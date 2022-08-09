8cf333475e8c1ec130b932cc52ff720c990a161f
rjl275 <rjl275@cornell.edu>
2022-08-08 20:10:59 -0700

    initialcommit

--- web.js
+++ web.js
@@ -34,8 +34,8 @@ async function load() {
     displayMemory()
 
     list = new Array(4096)
-    for (let i = 0; i < romBuffer.data.length; i += 2) {
-        list.splice(0x200 + i, 0, cpu.decode(cpu.memory[0x200 + i] << 8 | cpu.memory[0x200 + i + 1] << 0)) //assign to some empty list
+    for (let i = 0; i < romBuffer.data.length; i++) {
+        list.splice(0x200 + i, 0, cpu.decode(cpu.memory[0x200 + i * 2] << 8 | cpu.memory[0x200 + i * 2 + 1] << 0)) //assign to some empty list
     }
 }
 
